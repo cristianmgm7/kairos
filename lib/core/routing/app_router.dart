28ac52c306a718aae048faab3a62b89df8620621
cristianmgm7 <cristianmgm7@gmail.com>
2025-10-28 18:04:15 -0500

    Refactor app routing to remove unused pages
    - Removed routing entries for SplashPage, OnboardingPage, LoginPage, and RegisterPage from AppRouter.
    - Updated the initial location to focus on the DashboardPage, streamlining the navigation structure.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,10 +1,6 @@
 import 'package:blueprint_app/core/routing/app_routes.dart';
 import 'package:blueprint_app/core/routing/pages/dashboard_page.dart';
 import 'package:blueprint_app/core/routing/pages/error_page.dart';
-import 'package:blueprint_app/core/routing/pages/login_page.dart';
-import 'package:blueprint_app/core/routing/pages/onboarding_page.dart';
-import 'package:blueprint_app/core/routing/pages/register_page.dart';
-import 'package:blueprint_app/core/routing/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:injectable/injectable.dart';
@@ -18,22 +14,6 @@ class AppRouter {
         initialLocation: AppRoutes.splash,
         debugLogDiagnostics: true,
         routes: [
-          GoRoute(
-            path: AppRoutes.splash,
-            builder: (context, state) => const SplashPage(),
-          ),
-          GoRoute(
-            path: AppRoutes.onboarding,
-            builder: (context, state) => const OnboardingPage(),
-          ),
-          GoRoute(
-            path: AppRoutes.login,
-            builder: (context, state) => const LoginPage(),
-          ),
-          GoRoute(
-            path: AppRoutes.register,
-            builder: (context, state) => const RegisterPage(),
-          ),
           GoRoute(
             path: AppRoutes.dashboard,
             builder: (context, state) => const DashboardPage(),
