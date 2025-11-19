4470dc4a5358f769890a44f23e985f6f1f97246c
yohanangulo <anguloyohan98@gmail.com>
2025-11-19 10:58:00 -0400

    Auth refactor

---
+++ lib/features/auth/domain/usecases/check_user_profile.dart
@@ -0,0 +1,21 @@
+import 'package:kairos/core/utils/result.dart';
+import 'package:kairos/features/profile/domain/repositories/user_profile_repository.dart';
+
+class CheckUserProfileUseCase {
+  CheckUserProfileUseCase(this._profileRepository);
+
+  final UserProfileRepository _profileRepository;
+
+  Stream<bool> call(String userId) async* {
+    final profile = await _profileRepository.getProfileByUserId(userId);
+
+    if (profile.dataOrNull == null) {
+      await _profileRepository.syncProfile();
+    }
+
+    yield* _profileRepository
+        .watchProfileByUserId(userId)
+        .distinct()
+        .map((profile) => profile != null);
+  }
+}
