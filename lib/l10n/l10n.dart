fbd5c0b9c7d261639aac6463e6796295365f7f85
yohanangulo <anguloyohan98@gmail.com>
2025-11-23 20:07:32 -0400

    Refactor localization handling by removing l10nProvider and using late initialization for AppLocalizations

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kairos/core/extensions/extensions.dart';
 import 'package:kairos/l10n/app_localizations.dart';
 
@@ -11,17 +10,14 @@ import 'package:kairos/l10n/app_localizations.dart';
 /// context.l10n.translation
 /// ```
 ///
-/// [l10nProvider] should be only used when [BuildContext] is not available
-final l10nProvider = Provider<AppLocalizations>((ref) {
-  throw UnimplementedError('l10nProvider provider must be overridden');
-});
+/// [l10n] should be only used when [BuildContext] is not available
+late AppLocalizations l10n;
 
 abstract class L10n {
   const L10n._();
 
   static String onGenerateTitle(BuildContext context) {
-    final l10n = context.l10n;
-    l10nProvider.overrideWithValue(l10n);
+    l10n = context.l10n;
 
     return 'Kairos';
   }
